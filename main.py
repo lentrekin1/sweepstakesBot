7fa642bdc0725347fdb4b61afa703fb6bfa77643
lance <lancee478@gmail.com>
2021-05-17 15:51:14 -0700

    removed heroku settings

--- main.py
+++ main.py
@@ -16,19 +16,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 email_opts = ['lj478654', 'xxbotmail69xx', 'robertbarr4891', 'dripgang010203']
 giveaway = "https://www.itsourfabfashlife.com/2021/05/ice-cream-tour-2021new-york-city.html"
 
-on_heroku = True if os.environ['ON_HEROKU'] == 'True' else False
 chrome_options = webdriver.ChromeOptions()
-if not on_heroku:
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--remote-debugging-port=9222')
+chrome_options.add_argument('--headless')
 
-if on_heroku:
-    chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
 fake = Faker()
-timeout = 15 if on_heroku else 5
-wait = 5 if on_heroku else 0.5
+timeout = 5
+wait = 0.5
 num_done = 0
 num_failed = 0
 
